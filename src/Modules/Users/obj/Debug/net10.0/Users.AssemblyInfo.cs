97e583667d135f7b340f9b863209d335faaf865c
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2026-08-12 00:10:01 +0600

    restructured

--- src/Modules/Users/obj/Debug/net10.0/Users.AssemblyInfo.cs
+++ src/Modules/Users/obj/Debug/net10.0/Users.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Users")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f53f701393677b7272e1c450b522b645d2d994b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444a7fe483074538445348dfb331329203a5d87a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Users")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Users")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
