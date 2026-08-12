c5e3eb3f11fa28246d66f4269949f1a1b727dd10
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2026-08-12 23:33:42 +0600

    added entity config class for profile entity

--- src/Modules/Users/Entities/ProfileEntity.cs
+++ src/Modules/Users/Entities/ProfileEntity.cs
@@ -4,25 +4,13 @@ using Shared.Infra.Enums;
 
 namespace Users.Entities;
 
-[Index(nameof(FirstName))]
-[Index(nameof(LastName))]
-[Index(nameof(Phone), IsUnique =true)]
 public class Profile
 {
-    [Key]
     public Guid Id {get; set;}
-
-    [MaxLength(100)]
     public string FirstName {get; set;} = string.Empty;
-
-    [MaxLength(100)]
     public string LastName {get; set;} = string.Empty;
-
-    [MaxLength(20)]
     public string Phone {get; set;} = string.Empty;
-
     public Gender Gender {get; set;} = Gender.Other;
-
     public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
     public DateTime UpdatedAt {get; set;} = DateTime.UtcNow;
 }
