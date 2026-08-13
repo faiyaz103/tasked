77cafce820eb02b6e26612ea86b487fafe3d3efd
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2026-08-13 22:30:54 +0600

    defined entity relationships with migration override

--- src/Modules/Tasks/Entities/TaskGroupEntity.cs
+++ src/Modules/Tasks/Entities/TaskGroupEntity.cs
@@ -4,6 +4,7 @@ public class TaskGroup
 {
     public Guid Id {get; set;}
     public string Title {get; set;} = string.Empty;
+    public Guid ProfileId {get; set;}
     public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
     public DateTime UpdatedAt {get; set;} = DateTime.UtcNow;
 
---
+++ src/Modules/Tasks/Migrations/20260813161546_AddOneProfileTOManyTaskGroups.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tasks.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOneProfileTOManyTaskGroups : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "profile_id",
+                table: "taskgroups",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "ix_taskgroups_profile_id",
+                table: "taskgroups",
+                column: "profile_id");
+
+            migrationBuilder.Sql(@"
+                ALTER TABLE taskgroups 
+                ADD CONSTRAINT fk_taskgroups_profiles_profile_id 
+                FOREIGN KEY (profile_id) 
+                REFERENCES profiles(id) 
+                ON DELETE CASCADE;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {   
+            migrationBuilder.Sql(@"
+                ALTER TABLE taskgroups 
+                DROP CONSTRAINT fk_taskgroups_profiles_profile_id;
+            ");
+
+            migrationBuilder.DropIndex(
+                name: "ix_taskgroups_profile_id",
+                table: "taskgroups");
+
+            migrationBuilder.DropColumn(
+                name: "profile_id",
+                table: "taskgroups");
+        }
+    }
+}
